024a4933831b670ab788c920e6c454c9cd3189de
Roy-Aleena <135068502+Roy-Aleena@users.noreply.github.com>
2024-09-03 14:55:04 -0400

    Update log_dataset.py

--- utils/loggers/wandb/log_dataset.py
+++ utils/loggers/wandb/log_dataset.py
@@ -1,17 +1,41 @@
 import argparse
-
+import yaml
 from wandb_utils import WandbLogger
-
 from utils.general import LOGGER
 
 WANDB_ARTIFACT_PREFIX = 'wandb-artifact://'
 
+def load_dataset_config(config_path):
+    """Load dataset configuration from a YAML file."""
+    with open(config_path, 'r') as file:
+        config = yaml.safe_load(file)
+    
+    num_classes = config['nc']
+    class_names = config['names']
+    
+    return num_classes, class_names
 
 def create_dataset_artifact(opt):
-    logger = WandbLogger(opt, None, job_type='Dataset Creation')  # TODO: return value unused
+    logger = WandbLogger(opt, None, job_type='Dataset Creation')
+    
     if not logger.wandb:
-        LOGGER.info("install wandb using `pip install wandb` to log the dataset")
+        LOGGER.info("Install wandb using `pip install wandb` to log the dataset")
+        return
 
+    # Load dataset configuration
+    num_classes, class_names = load_dataset_config(opt.data)
+    
+    # Example log the dataset info to W&B
+    logger.wandb.log({
+        'dataset_config': {
+            'path': opt.data,
+            'num_classes': num_classes,
+            'class_names': class_names
+        }
+    })
+
+    LOGGER.info(f"Dataset configuration loaded: {num_classes} classes")
+    LOGGER.info(f"Class names: {', '.join(class_names)}")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
